2e787e1b68fa01b521c7fcb3f68fbd57c0ce6e5c
Musab36 <musabhd@gmail.com>
2017-10-19 15:32:20 +0300

    Initial commit

---
+++ app/src/main/java/sandule/com/chatmaster/UserDetails.java
@@ -0,0 +1,11 @@
+package sandule.com.chatmaster;
+
+/**
+ * Created by Musab on 10/19/2017.
+ */
+
+public class UserDetails {
+    static String username = "";
+    static String password = "";
+    static String chatWith = "";
+}
